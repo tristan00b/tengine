1a26216b4ad2fdaccb5d202f175aedc4c409d424
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-12-24 15:11:17 -0800

    Add initial type utilities

---
+++ test/engine.util.Types.spec.ts
@@ -0,0 +1,26 @@
+import { fromHex,
+         toHex    } from '@engine/util/Types'
+
+
+describe('engine.util.Types', () => {
+
+  const ival = 32
+  const hexstr = '0x20'
+
+  describe('engine.util.Types.fromHex', () => {
+
+    test('Converts a hexadecimal string to a number', () => {
+      expect(fromHex(hexstr)).toBe(ival)
+    })
+  })
+
+
+  describe('engine.util.Types.toHext', () => {
+
+    test('Converts a numeric value to a hexadecimal string', () => {
+      expect(toHex(ival)).toBe(hexstr)
+    })
+
+  })
+
+})
