4e2d84af96de1dad0fa3e81b3d79d3dc379cb9ff
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-01-03 07:28:40 -0800

    Prefer Error instance over interface

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,23 @@ import { Error,
          fail,
          type IError } from '@engine/util/Errors'
 
+
 window.addEventListener('load', async () => {
   try {
-    const canvas  = document.querySelector('#game-canvas') as HTMLCanvasElement ?? fail({ kind: 'APPLICATION_ERROR', message: 'failed to locate document canvas element' })
-    const context = canvas?.getContext('webgl2') ?? fail({ kind: 'APPLICATION_ERROR', message: 'failed to acquire WebGL2 rendering context' })
-    const game    = new Game(context, { loopOnce: false, showDebugInfo: true })
+    const canvas: HTMLCanvasElement =
+      document.querySelector('#game-canvas') ??
+        fail(new Error('FATAL_ERROR', 'failed to locate document canvas element'))
+
+    const context: WebGL2RenderingContext =
+      canvas?.getContext('webgl2') ??
+        fail(new Error('FATAL_ERROR', 'failed to acquire WebGL2 rendering context'))
+
+    const game = new Game(context, { loopOnce: false, showDebugInfo: true })
 
     game.start()
   }
   catch (err)
   {
-    console.error(Error.toString(err as IError<any>))
+    console.error(Error.toString(err as IError<string>))
   }
 })
