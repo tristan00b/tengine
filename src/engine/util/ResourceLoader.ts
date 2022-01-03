ceb84f60a44104b73305e1c7574c2e5ba946e319
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-01-03 08:37:32 -0800

    Add new methods for loading resources via rel paths

--- src/engine/util/ResourceLoader.ts
+++ src/engine/util/ResourceLoader.ts
@@ -1,6 +1,22 @@
-import { Error,
+import { concatErrors,
+         Error,
+         fail,
          isError,
          type IError } from '@engine/util/Errors'
+
+/**
+ * Gets the origin from which application is being hosted:
+ *  - `globalThis.location` if executed in a browser
+ *  - `process.env.BASE_URL if executed in node
+ *     (i.e. corresponding to the base URL of the test server)
+ * @returns The base URL of the local host
+ */
+export function getBaseUrl(): string | IError<'CONFIG_ERROR'>
+{
+  return (globalThis?.location as unknown) as string
+    ?? process?.env?.BASE_URL
+    ?? new Error('CONFIG_ERROR', 'failed to determine baseUrl')
+}
 
 /**
  * Returns true if the response's status code is 200
@@ -14,10 +30,22 @@ export const isSuccessResponse = (res: Response) =>
  */
 export class ResourceLoader
 {
+  private static _baseUrl: string
+  static get baseUrl() { return this._baseUrl }
+
+  static {
+    const result = getBaseUrl()
+    this._baseUrl  = !isError(result)
+      ? result
+      : fail(
+        concatErrors(new Error('FATAL_ERROR', 'failed to determin baseUrl'), result)
+      )
+  }
+
   /**
    * A wrapper for the Javascript `fetch` global method
-   * @param url The location to fetch from (see [here](https://developer.mozilla.org/en-US/docs/Web/API/URL/URL) for details).
-   * @param options Request init options (see [here](https://developer.mozilla.org/en-US/docs/Web/API/Request/Request) for details).
+   * @param url The location to fetch from (see [here](https://developer.mozilla.org/en-US/docs/Web/API/URL/URL) for details)
+   * @param options Request init options (see [here](https://developer.mozilla.org/en-US/docs/Web/API/Request/Request) for details)
    * @returns
    */
   static async load(url: URL, options?: RequestInit): Promise<Response|IError<'FETCH_ERROR'>>
@@ -26,7 +54,32 @@ export class ResourceLoader
       .then((response: Response) =>
         isSuccessResponse(response)
           ? response
-          : new Error('FETCH_ERROR', `${response.status}: ${response.statusText}`)
+          : new Error('FETCH_ERROR', `received [${response.status} - ${response.statusText}] for address ${url.href}`)
       )
   }
+
+  /**
+   * Loads a file relative to the root directory from which the application is served.
+   * @param path The relative path of the resource to fetch
+   * @param options Request init options (see [here](https://developer.mozilla.org/en-US/docs/Web/API/Request/Request) for details)
+   * @returns
+   */
+  static async loadLocal(path: string, options?: RequestInit)
+  {
+    const url = new URL(path, this.baseUrl)
+    return this.load(url, options)
+  }
+
+  /**
+   *
+   * @param path
+   * @param options
+   * @returns
+   */
+  static async loadAsset(path: string, options?: RequestInit)
+  {
+    const url = new URL(`/assets/${path}`, this.baseUrl)
+    return this.load(url, options)
+  }
+
 }
--- test/live/engine.util.ResourceLoader.spec.ts
+++ test/live/engine.util.ResourceLoader.spec.ts
@@ -5,10 +5,20 @@ describe('engine.ResourceLoader', () => {
 
   const baseUrl = `${process.env.BASE_URL}`
 
-  test('it loads a resource file', async () => {
+  test('it resolves the correct baseUrl', () => {
+    expect(ResourceLoader.baseUrl).toBe(baseUrl)
+  })
+
+  test('it loads a local file', async () => {
     return ResourceLoader
-      .load(new URL('/assets/shaders/basic.vert', baseUrl), { headers: { 'Content-Type': 'text/plain' }})
+      .loadLocal('app.config.json', { headers: { 'Content-Type': 'application/json' }})
+      .then(async resp => expect(isError(resp)).toBe(false))
+  })
+
+  test('it loads an asset file', () => {
+    return ResourceLoader
+      .loadAsset('shaders/basic.vert', { headers: { 'Content-Type': 'text/plain' }})
       .then(async resp => isError(resp) ? fail(resp) : await resp.text())
-      .then(async text => expect(text.startsWith('#version 300 es')).toBeTruthy())
+      .then(async text => expect(text.startsWith('#version 300 es')).toBe(true))
   })
 })
