f98b65a5b9f24b6b955c508aee3f3e6663565d20
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-03-04 02:46:54 -0800

    Move error types into Error.ts

--- src/engine/util/Error.ts
+++ src/engine/util/Error.ts
@@ -1,15 +1,14 @@
-import { head,
-         split            } from './Utilities'
+import { head
+       , split            } from './Utilities'
+import { fail
+       , isError
+       , isAggregateError } from 'typey-doo'
 
-import { fail,
-         isError,
-         isAggregateError } from 'typey-doo'
 
-export { fail,
-         isError,
-         isAggregateError }
+export { fail
+       , isError
+       , isAggregateError }
 
-export * from './error/types'
 
 /** Takes an error and returns a string representation in the form "<ErrorTypeName>: <ErrorMessage>" */
 export function showError<E extends Error>(error: E): string
@@ -61,4 +60,96 @@ export function trace<T>(it: T): T
 {
   console.log(it)
   return it
+}
+
+
+/* ------------------------------------------------------------------------------------------------------------------ *\
+   Custom Error types
+\* ------------------------------------------------------------------------------------------------------------------ */
+
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+export class BufferError extends Error
+{
+}
+
+export interface BufferError extends Error
+{
+}
+
+export interface BufferErrorConstructor extends ErrorConstructor
+{
+}
+
+export class ConfigError extends Error
+{
+}
+
+export interface ConfigError extends Error
+{
+}
+
+export interface ConfigErrorConstructor extends ErrorConstructor
+{
+}
+
+export class FatalError extends Error
+{
+}
+
+export interface FatalError extends Error
+{
+}
+
+export interface FatalErrorConstructor extends ErrorConstructor
+{
+}
+
+export class FetchError extends Error
+{
+}
+
+export interface FetchError extends Error
+{
+}
+
+export interface FetchErrorConstructor extends ErrorConstructor
+{
+}
+
+
+export class NotImplementedError extends Error
+{
+}
+
+export interface NotImplementedError extends Error
+{
+}
+
+export interface NotImplementedErrorConstructor extends ErrorConstructor
+{
+}
+
+export class ShaderError extends Error
+{
+}
+
+export interface ShaderError extends Error
+{
+}
+
+export interface ShaderErrorConstructor extends ErrorConstructor
+{
+}
+
+export class TextureError extends Error
+{
+}
+
+export interface TextureError extends Error
+{
+}
+
+export interface TextureErrorConstructor extends ErrorConstructor
+{
 }
